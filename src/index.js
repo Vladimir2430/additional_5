151480f23a1aab9271d6084088b108a76e36db60
Vladimir <31570004+Vladimir2430@users.noreply.github.com>
2017-09-19 12:51:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ module.exports = function check(str, bracketsConfig) {
         count = 0;
     for (var i = 0; i < len; i++) {
         for (var j = 0; j < bracketsConfig.length; j++) {
-            if (str.char(i) === bracketsConfig[j][0]) {
-                arr.push(str.char(i));
+            if (str.charAt(i) === bracketsConfig[j][0]) {
+                arr.push(str.charAt(i));
             }
-            if (str.char(i) === bracketsConfig[j][1]) {
+            if (str.charAt(i) === bracketsConfig[j][1]) {
                 if (arr[arr.length - 1] === bracketsConfig[j][0]) {
                     arr.pop();
                 }
