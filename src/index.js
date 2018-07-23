1b027573a602809d6ce79cf617e71b6f45466bd8
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-23 19:26:50 +0300

    code improved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-    var arr=[];
-    var count=0;
-    for (let i=0; i<str.length; i++) {
-        for (let j=0; j<bracketsConfig.length; j++) {
-            if (str.charAt(i)==bracketsConfig[j][0] || str.charAt(i)==bracketsConfig[j][1]) {
-                if (bracketsConfig[j][0]==bracketsConfig[j][1]) {
-                    if (arr[arr.length-1]==bracketsConfig[j][0]) {
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str.charAt(i) === bracketsConfig[j][0] || str.charAt(i) === bracketsConfig[j][1]) {
+                if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+                    if (arr[arr.length - 1] === bracketsConfig[j][0]) {
                         arr.pop();
                         break;
                     }
@@ -13,19 +12,18 @@ module.exports = function check(str, bracketsConfig) {
                     break;
                 }
 
-                if(str[i]==bracketsConfig[j][0]) {
+                if(str[i] === bracketsConfig[j][0]) {
                     arr.push(str.charAt(i));
                     break;
                 }
 
-                if (arr[arr.length-1]!=bracketsConfig[j][0] || arr.length==0) {
+                if (arr[arr.length - 1] !== bracketsConfig[j][0] || arr.length === 0) {
                     return false;
                 }
 
                 arr.pop();
-                break;
             }
         }
     }
-    return (arr.length==0)?true:false;
+    return arr.length == 0 ? true : false;
 }
